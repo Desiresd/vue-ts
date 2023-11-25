9bd0db72efac78e30aa07cf6f7c130e53f482c12
MO <1192940849@qq.com>
2023-11-25 21:35:01 +0800

    增加ts类型检查

--- src/views/exercises/01_并发任务控制.vue
+++ src/views/exercises/01_并发任务控制.vue
@@ -12,13 +12,17 @@ function timeout(time: number) {
 }
 
 class SuperTask {
+  parallelCount: number;
+  tasks: Array<Object>;
+  runningCount: number;
+
   constructor(parallelCount = 2) {
     this.parallelCount = parallelCount; // 并发数量
     this.tasks = [];
     this.runningCount = 0; // 正在执行的任务数量
   }
 
-  add(task) {
+  add(task: any) {
     return new Promise((resolve, reject) => {
       this.tasks.push({
         task,
@@ -33,7 +37,7 @@ class SuperTask {
   _run() {
     console.log(`任务队列数量${this.tasks.length}`);
     while (this.runningCount < this.parallelCount && this.tasks.length > 0) {
-      const { task, resolve, reject } = this.tasks.shift();
+      const { task, resolve, reject }: any = this.tasks.shift();
       this.runningCount++;
       task()
         .then(resolve, reject)
